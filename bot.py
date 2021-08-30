98c60cbcc7e146c173f4c793b912481fa3580be6
a.b.vasilyev <vasilyev@unim.su>
2021-08-30 21:23:58 +0300

    -

--- bot.py
+++ bot.py
@@ -24,11 +24,16 @@ logging.basicConfig(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
+# chat_id = update.effective_chat.id - получить chat.id текущего пользователя
+
 
 def bot_commands(update, _):
     """ Доступные команды для работы с ботом """
-    update.message.reply_text("/start - старт бота\n/subscribe - "
-                              "получать уведомление о новом расписании\n"
+    update.message.reply_text("/start - старт бота\n"
+                              "Я проверяю наличие нового расписания"
+                              " каждые 2 часа!!! Хочешь получать уведомления?\n"
+                              "/subscribe - получать уведомление"
+                              " о новом расписании\n"
                               "/unsubscribe - отписаться от уведомлений\n"
                               "автор бота @drbrch,"
                               " принимаю в лс предложения/замечания")
@@ -67,16 +72,41 @@ def unsubscribe(update, _):
     update.message.reply_text('Уведомления о новом расписании отключены')
 
 
+def greet_user(update, _):
+    """ Приветствует пользователя, выводит основную клавиатуру """
+    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
+    print("Вызван /start")
+    update.message.reply_text("Привет, чтобы узнать расписание 1 корпуса\n"
+                              "нажми нужную тебе кнопку\n"
+                              "Уведомления об обновлении расписания\n"
+                              "можно подключить в 'Команды бота'",
+                              reply_markup=main_keyboard())
+
+
+def show_rings(update, context):
+    """ Выводит список звонков. Также будет выводить в случайном порядке
+     любые jpeg файлы в каталоге images """
+    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
+    rings_list = glob.glob('images/rings*.jp*g')
+    # rings_pic_filename = choice(rings_list) отправка случайного jpeg
+    for picture in rings_list:
+        context.bot.send_photo(chat_id=user['chat_id'],
+                               photo=open(picture, 'rb'),
+                               reply_markup=main_keyboard())
+
+
 def sending_notify_about_updating_schedules(schedule_folder, context):
+    """ Присылает уведомления, подписанным на уведомления пользоватеям """
     for user in get_subsribed(db):
         try:
             context.bot.send_message(chat_id=user['chat_id'],
                                      text=f'{schedule_folder} обновлено')
-        except Unauthorized:
+        except Unauthorized:    # ловим ошибку, если юзер заблокировал бота
+            # выводим в консоль юзеров, заблокировавших бота
             print(f"Пользователь {user['chat_id']} заблокировал бота")
 
 
-def parsing_links_from_schedule_html_downloading_schedules(context):
+def parsing_links_from_schedule_html_downloading_schedules(context, user=False):
     """ Парсит страницу DATASET_URL, ищет ссылку на Изменения к основному
      расписанию, вызывает функцию скачивания downloading_schedules, передавая
      в нее параметры, необходимые для скачивания Изменений.
@@ -84,6 +114,10 @@ def parsing_links_from_schedule_html_downloading_schedules(context):
      downloading_schedules, передавая в нее параметры, необходимые для
      скачивания Основного """
     print('*** Start parsing ***')
+    global NAME_OF_CHANGES_SCHEDULE_FILE
+    NAME_OF_CHANGES_SCHEDULE_FILE = ''
+    global NAME_OF_MAIN_SCHEDULE_FILE
+    NAME_OF_MAIN_SCHEDULE_FILE = ''
     DATASET_URL = "https://ptgh.onego.ru/9006/"
     url = Request(DATASET_URL)
     html_page = urlopen(url)
@@ -96,34 +130,31 @@ def parsing_links_from_schedule_html_downloading_schedules(context):
         if (link.find('ismen_nov')) != -1:
             changes_schedule_link = link
             changes_schedule = urlparse(changes_schedule_link)
-            global NAME_OF_CHANGES_SCHEDULE_FILE
             NAME_OF_CHANGES_SCHEDULE_FILE = changes_schedule.path.replace(
                 '/', '')
             print(NAME_OF_CHANGES_SCHEDULE_FILE)
             # передаем ссылку по которой качать, название папки,
             # имя скачиваемого файла, context
-            downloading_schedules(
+            download_result = downloading_schedules(
                 changes_schedule_link, 'Изменения к расписанию',
-                                  NAME_OF_CHANGES_SCHEDULE_FILE
-            )
-            sending_notify_about_updating_schedules(
-                'Изменения к расписанию', context)
+                                  NAME_OF_CHANGES_SCHEDULE_FILE)
+            if download_result is True and user is False:
+                sending_notify_about_updating_schedules(
+                    'Изменения к расписанию', context)
             continue        # переход к следующей итерации
         if re.search(r'РАСПИСАНИЕ.*\.xlsx', link):
             main_schedule_link = link
             main_schedule = urlparse(main_schedule_link)
-            global NAME_OF_MAIN_SCHEDULE_FILE
             NAME_OF_MAIN_SCHEDULE_FILE = main_schedule.path.replace('/', '')
             print(NAME_OF_MAIN_SCHEDULE_FILE)
             # передаем ссылку по которой качать, название папки,
             # имя скачиваемого файла, context
-            if downloading_schedules(
+            download_result = downloading_schedules(
                     main_schedule_link, 'Основное расписание',
-                    NAME_OF_MAIN_SCHEDULE_FILE) is None:
-                break
-            else:
+                    NAME_OF_MAIN_SCHEDULE_FILE)
+            if download_result is True and user is False:
                 sending_notify_about_updating_schedules(
-                    'Основное расписание', context)
+                  'Основное расписание', context)
 
 
 def downloading_schedules(
@@ -142,6 +173,7 @@ def downloading_schedules(
         f.write(schedule_xlsx.content)  # записывает content из get запроса
         f.close()
         print(f'Расписание скачано в {schedule_folder}')
+        return True  # запомним результат как True, если функция отработала
     elif os.listdir(schedule_folder)[0] != name_of_schedule_file:
         os.remove(schedule_folder + '/' + os.listdir(schedule_folder)[0])
         schedule_xlsx = requests.get(schedule_link)
@@ -149,68 +181,66 @@ def downloading_schedules(
         f.write(schedule_xlsx.content)  # записывает content из get запроса
         f.close()
         print(f'Расписание обновлено в {schedule_folder}')
-    else:
-        return None
-
-
-def greet_user(update, _):
-    """ Приветствует пользователя, выводит основную клавиатуру """
-    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
-    print("Вызван /start")
-    update.message.reply_text("Привет, чтобы узнать расписание нажми кнопку\n"
-                              "Уведомления об обновлении расписания\n"
-                              "можно подключить в 'Команды бота'",
-                              reply_markup=main_keyboard())
-
-
-# chat_id = update.effective_chat.id - получить chat.id текущего пользователя
-
-
-def show_rings(update, context):
-    """ Выводит список звонков. Также будет выводить в случайном порядке
-     любые jpeg файлы в каталоге images """
-    user = get_or_create_user(db, update.effective_user, update.message.chat.id)
-    rings_list = glob.glob('images/rings*.jp*g')
-    rings_pic_filename = choice(rings_list)
-    context.bot.send_photo(chat_id=user['chat_id'],
-                           photo=open(rings_pic_filename, 'rb'),
-                           reply_markup=main_keyboard())
+        return True  # запомним результат как True, если функция отработала
+    return False  # запомним результат как False, если функция не отработала
 
 
 def send_main_schedule(update, context):
-    #parsing_links_from_schedule_html_downloading_schedules(context)
+    parsing_links_from_schedule_html_downloading_schedules(context, user=True)
     user = get_or_create_user(db, update.effective_user, update.message.chat.id)
-    context.bot.send_document(
-        chat_id=user['chat_id'], document=open(
-            'Основное расписание/' + NAME_OF_MAIN_SCHEDULE_FILE, 'rb'),
-        reply_markup=main_keyboard()
-    )
+    if NAME_OF_MAIN_SCHEDULE_FILE == '':
+        update.message.reply_text('Основное расписание не найдено на сайте, '
+                                  'но у меня есть старая версия,'
+                                  ' используй внимательно')
+        context.bot.send_document(
+            chat_id=user['chat_id'], document=open(
+                'Основное расписание/' + os.listdir('Основное расписание')[0],
+                'rb'),
+            reply_markup=main_keyboard())
+    else:
+        context.bot.send_document(
+            chat_id=user['chat_id'], document=open(
+                'Основное расписание/' + NAME_OF_MAIN_SCHEDULE_FILE, 'rb'),
+            reply_markup=main_keyboard()
+        )
 
 
 def send_changes_schedule(update, context):
-    parsing_links_from_schedule_html_downloading_schedules(context)
+    parsing_links_from_schedule_html_downloading_schedules(context, user=True)
     user = get_or_create_user(db, update.effective_user, update.message.chat.id)
-    context.bot.send_document(
-        chat_id=user['chat_id'], document=open(
-            'Изменения к расписанию/' + NAME_OF_CHANGES_SCHEDULE_FILE, 'rb'),
-        reply_markup=main_keyboard()
-    )
+    if NAME_OF_CHANGES_SCHEDULE_FILE == '':
+        update.message.reply_text('Изменений расписания не найдено на сайте, '
+                                  'но у меня есть старая версия,'
+                                  ' используй внимательно')
+        context.bot.send_document(
+            chat_id=user['chat_id'], document=open(
+                'Изменения к расписанию/' + os.listdir(
+                    'Изменения к расписанию')[0], 'rb'),
+            reply_markup=main_keyboard())
+    else:
+        context.bot.send_document(
+            chat_id=user['chat_id'], document=open(
+                'Изменения к расписанию/' + NAME_OF_CHANGES_SCHEDULE_FILE,
+                'rb'), reply_markup=main_keyboard())
 
 
 def show_dates_of_changes_schedule(update, _):
     """ Выводит список дат, доступных для выбора """
-    book = openpyxl.open(
+    if NAME_OF_CHANGES_SCHEDULE_FILE == '':
+        update.message.reply_text('Изменений расписания не найдено на сайте')
+    else:
+        book = openpyxl.open(
         'Изменения к расписанию/' + NAME_OF_CHANGES_SCHEDULE_FILE,
         read_only=True
-    )
-    my_keyboard = ReplyKeyboardMarkup(
-        # выводим кнопки с датами и кнопку отмена
-        [book.sheetnames, ['Отмена']], resize_keyboard=True)
-    update.message.reply_text(
-        "Изменения к расписанию занятий Корпус 1 (ул. Мурманская, д. 30)\n"
-        "выберите дату:",
-        reply_markup=my_keyboard)
-    return "step_one"
+        )
+        my_keyboard = ReplyKeyboardMarkup(
+            # выводим кнопки с датами и кнопку отмена
+            [book.sheetnames, ['Отмена']], resize_keyboard=True)
+        update.message.reply_text(
+            "Изменения к расписанию занятий Корпус 1 (ул. Мурманская, д. 30)\n"
+            "выберите дату:",
+            reply_markup=my_keyboard)
+        return "step_one"
 
 
 def choose_sheet_of_changes_schedule(update, context):
@@ -226,9 +256,13 @@ def choose_sheet_of_changes_schedule(update, context):
     sheet = book[context.user_data["dialog"]["sheet"]]
     parsed_schedule = system_functions.parsing_changes_xlsx(sheet)
     groups = re.compile(
-        r'(БД 12)|(БД 22)|(ИС 11)|(ИС 21)|(ИС 31)|(В 01)|(В 21)|(М 01)|(ПД 12)|'
-        r'(ПД 13)|(ПД 22)|(ПД 23)|(ПД 24)|(ПД 32)|(ПД 33)|(ПД 34)|(ПСО 11)|'
-        r'(ПСО 12)|(ПСО 21)|(ПСО 22)|(ПСО 31)|(ПСО 32)|(Т 11)|(Т 21)|(Т 31)|'
+        r'(БД 12)|(БД 22)|(ИС 11)|(ИС 21)|(ИС 31)|'
+        r'(В 01)|(В 11)|(В 21)|(В 31)|'
+        r'(ЗИ 11)|'
+        r'(М 01)|(M 11)|(М 21)|(М 31)|'
+        r'(ПД 12)|(ПД 13)|(ПД 22)|(ПД 23)|(ПД 24)|(ПД 32)|(ПД 33)|(ПД 34)|'
+        r'(ПСО 11)|(ПСО 12)|(ПСО 21)|(ПСО 22)|(ПСО 31)|(ПСО 32)|'
+        r'(Т 11)|(Т 21)|(Т 31)|'
         r'(УД 01)|(УД 11)|(УД 21)|(УД 31)|(Э 12)|(Э 22)')
     idx1 = [i for i, item in enumerate(parsed_schedule) if
             re.search(groups, item)]
@@ -293,9 +327,16 @@ def main():
                 choose_sheet_of_changes_schedule)],
             "step_two": [
                 MessageHandler(Filters.regex(
-                '(([ЭМТВ].[0-4][0-4])|([ЭМТВ].[0-4])|([БУИП][ДС].[0-4][0-4]|'
-                '[ИБПУ][СД].[0-4])|([П][С][О].[0-4][0-4])|([ВМ][0-4]|'
-                '[У][Д][0-4]))'), print_changes_schedule)]
+                    r'(БД 12)|(БД 22)|(ИС 11)|(ИС 21)|(ИС 31)|'
+                    r'(В 01)|(В 11)|(В 21)|(В 31)|'
+                    r'(ЗИ 11)|'
+                    r'(М 01)|(M 11)|(М 21)|(М 31)|'
+                    r'(ПД 12)|(ПД 13)|(ПД 22)|(ПД 23)|(ПД 24)|'
+                    r'(ПД 32)|(ПД 33)|(ПД 34)|'
+                    r'(ПСО 11)|(ПСО 12)|(ПСО 21)|(ПСО 22)|(ПСО 31)|(ПСО 32)|'
+                    r'(Т 11)|(Т 21)|(Т 31)|'
+                    r'(УД 01)|(УД 11)|(УД 21)|(УД 31)|(Э 12)|(Э 22)'),
+                    print_changes_schedule)]
         },
         fallbacks=[
             # при нажатии кнопки отмена выходим из диалога
@@ -303,9 +344,9 @@ def main():
             MessageHandler(
                 # фильтры не дают пользователю ввести лишний текст, прислать
                 # фото, видео, геолокацию
-            Filters.text | Filters.video | Filters.photo | Filters.document
-          | Filters.location, show_dontunderstand)]
-    )
+                Filters.text | Filters.video | Filters.photo | Filters.document
+                | Filters.location, show_dontunderstand)]
+            )
     dp.add_handler(dialog)
     dp.add_handler(CommandHandler('subscribe', subscribe))
     dp.add_handler(CommandHandler('unsubscribe', unsubscribe))
